7b43fd50bcf893e305f86a473de0bbd4136552e1
Pratapsaurabh <91468185+Pratapsaurabh@users.noreply.github.com>
2023-12-18 18:24:25 +0000

    basic 1 done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -13,3 +13,24 @@ console.log(myStr.toUpperCase())
 
 console.log(myStr.charAt(2));
 console.log(myStr.indexOf('p'));
+
+const newString = myStr.substring(0, 4) // can't give -ve value in index
+
+console.log(newString)
+
+const anotherString = myStr.slice(-6, 4)
+console.log(anotherString)
+
+// Trim()
+// will remove unwanted space 
+const newStringOne = "  sp  "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://sp.com/sp%20singh";
+console.log(url.replace('%20', '-'))
+console.log(url.includes('bat'))
+
+const varString = "sp-singh"
+console.log(varString.split('-')) // will split and make an array
+
